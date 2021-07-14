f89a82a79d91dc43deee782300c01670a52833f7
Jonathan Kelly <coder187@hotmail.com>
2021-07-14 16:56:53 +0100

    add gmail .net quickstart

--- ReadEmails/Program.cs
+++ ReadEmails/Program.cs
@@ -1,15 +1,76 @@
-﻿using System;
+﻿//14072021
+//jkelly@jsd.ie
+//Jonathan Kelly
+//coder187@hotmail.com
+//program to read email or emails from given gmail inbox or email id and then 
+//perist to db or otherwise make data available to caller...
+
+//https://developers.google.com/gmail/api/quickstart/dotnet
+
+
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Gmail.v1;
+using Google.Apis.Gmail.v1.Data;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+using System;
 using System.Collections.Generic;
-using System.Text;
+using System.IO;
+using System.Threading;
 
 namespace ReadEmails
 {
     class Program
     {
+        // If modifying these scopes, delete your previously saved credentials
+        // at ~/.credentials/gmail-dotnet-quickstart.json
+        static string[] Scopes = { GmailService.Scope.GmailReadonly };
+        static string ApplicationName = "Gmail API .NET Quickstart";
         static void Main(string[] args)
         {
-            Console.WriteLine("hello world!");
-            Console.ReadKey();
+            UserCredential credential;
+
+            using (var stream =
+                new FileStream("credentials.json", FileMode.Open, FileAccess.Read))
+            {
+                // The file token.json stores the user's access and refresh tokens, and is created
+                // automatically when the authorization flow completes for the first time.
+                string credPath = "token.json";
+                credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    GoogleClientSecrets.Load(stream).Secrets,
+                    Scopes,
+                    "user",
+                    CancellationToken.None,
+                    new FileDataStore(credPath, true)).Result;
+                Console.WriteLine("Credential file saved to: " + credPath);
+            }
+
+            // Create Gmail API service.
+            var service = new GmailService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = ApplicationName,
+            });
+
+            // Define parameters of request.
+            UsersResource.LabelsResource.ListRequest request = service.Users.Labels.List("me");
+
+            // List labels.
+            IList<Label> labels = request.Execute().Labels;
+            Console.WriteLine("Labels:");
+            if (labels != null && labels.Count > 0)
+            {
+                foreach (var labelItem in labels)
+                {
+                    Console.WriteLine("{0}", labelItem.Name);
+                }
+            }
+            else
+            {
+                Console.WriteLine("No labels found.");
+            }
+            Console.Read();
+
         }
     }
 }
