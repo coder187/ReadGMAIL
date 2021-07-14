7e99179b6d0d5824fb5ba194e6deba4e4b499d4d
Jonathan Kelly <coder187@hotmail.com>
2021-07-14 16:03:18 +0100

    initial commit

---
+++ ReadEmails/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ReadEmails
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello world!");
+            Console.ReadKey();
+        }
+    }
+}
