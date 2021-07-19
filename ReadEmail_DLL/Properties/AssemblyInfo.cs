65f426b892e9bbac154015b0daf0bbcbf2e45e63
Jonathan Kelly <coder187@hotmail.com>
2021-07-19 18:10:24 +0100

    compile for com interop

--- ReadEmail_DLL/Properties/AssemblyInfo.cs
+++ ReadEmail_DLL/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("ReadEmail_DLL")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("read gmail mail")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Jonathan Kelly")]
 [assembly: AssemblyProduct("ReadEmail_DLL")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
@@ -17,7 +17,7 @@ using System.Runtime.InteropServices;
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
+[assembly: ComVisible(true)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("e9632d0b-906b-4a39-a54c-b9fa32e67de0")]
